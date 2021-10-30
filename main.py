519795a7d3c57d8eee3f0ec2922cf2bec35ae70e
Hester <h.steyn@sanbi.org.za>
2021-10-30 14:39:09 +0200

    Steps added

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # this is the main script file that will do all the work
 # lets start by adding the processing steps here, as comments. We will then fill in the code
+
+#extract records form BODATSA in csv file
+
+
+#open/read csv file
+
+
+#loop through botanical records in csv
+
+
+#check each record's qds against QDSProvinces.csv
+
+
+
+#update record's qds according to QDSProvinces.csv
+
+
+#write unique identifier and updated provinces to new csv file
+
+
+#push updates back into BODATSA
