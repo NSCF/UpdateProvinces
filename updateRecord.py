8252e41f7a8c8ce8339408a75411ad13984ff3e3
Hester <h.steyn@sanbi.org.za>
2021-11-15 12:42:43 +0200

    update code

--- updateRecord.py
+++ updateRecord.py
@@ -1,32 +1,4 @@
 
-
-
-
-
-# def updateRecord(records, prov1, prov2):
-#     # for record in records:
-#     if len(records['QDS']) <=6:
-#         record['Check'] = 'y'
-#     elif prov1 == prov2: #if they match just update
-#             record['updatedProv'] = prov1   #add updated province
-#             record['Check'] = ''
-           
-#     else: #if province from QDS only, record and mark to check
-#         if prov1 or prov2 is None:
-#             if prov2 is None:
-#                 record['updatedProv'] = prov1
-#                 record['Check'] = 'y' 
-#             else: #if province from town only, then if exact match use the province, else use the province and mark for check
-#                 record['updatedProv'] = prov2
-#                 record['Check'] = 'y'
-#         else: #if they don't match, record both and mark to check
-#             updateProv['updatedProv'] = prov1 + '|' + prov2
-#             record['Check'] = 'y'
-        
-#     return updateProv
-    #return record
-
-
 def updateRecord(record, prov1, prov2):
     if prov1 == prov2: #if they match just update
             record['updatedProv'] = prov1   #add updated province
