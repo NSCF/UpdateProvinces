0936ff30e435f43d302bb2af465f5e51f85426b0
Hester <h.steyn@sanbi.org.za>
2021-11-15 12:38:15 +0200

    update function

--- updateRecord.py
+++ updateRecord.py
@@ -3,18 +3,46 @@
 
 
 
-def updateRecords(records, prov1, prov2):
-    # for record in records:
-        if len(record.QDS) <=6:
-            check = 'y'
-        else:
-            if prov1 == prov2:
-                updateProv = prov1
-                # OR? record.MAJORAREA = prov1
-            else:
-                updateProv = prov1 + ', ' + prov2
-                # OR? record.MAJORAREA = prov1 + ', ' + prov2
-                check = 'y'
-    return updateProv
+# def updateRecord(records, prov1, prov2):
+#     # for record in records:
+#     if len(records['QDS']) <=6:
+#         record['Check'] = 'y'
+#     elif prov1 == prov2: #if they match just update
+#             record['updatedProv'] = prov1   #add updated province
+#             record['Check'] = ''
+           
+#     else: #if province from QDS only, record and mark to check
+#         if prov1 or prov2 is None:
+#             if prov2 is None:
+#                 record['updatedProv'] = prov1
+#                 record['Check'] = 'y' 
+#             else: #if province from town only, then if exact match use the province, else use the province and mark for check
+#                 record['updatedProv'] = prov2
+#                 record['Check'] = 'y'
+#         else: #if they don't match, record both and mark to check
+#             updateProv['updatedProv'] = prov1 + '|' + prov2
+#             record['Check'] = 'y'
+        
+#     return updateProv
     #return record
 
+
+def updateRecord(record, prov1, prov2):
+    if prov1 == prov2: #if they match just update
+            record['updatedProv'] = prov1   #add updated province
+            record['Check'] = ''
+           
+    elif prov1 or prov2 is None:
+        if prov2 is None: #if province from QDS only, record and mark to check
+            record['updatedProv'] = prov1
+            record['Check'] = 'y' 
+        else: #I DON"T KNOW HOW TO TEST FOR THIS _ if province from town only, then if exact match use the province, else use the province and mark for check
+            record['updatedProv'] = prov2
+            record['Check'] = 'y'
+        
+    else: #if they don't match, record both and mark to check
+        updateProv['updatedProv'] = prov1 + '|' + prov2
+        record['Check'] = 'y'
+        
+    return updatedProv
+    #return record
